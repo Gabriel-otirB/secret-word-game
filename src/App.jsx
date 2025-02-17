27516755e6866c30f22eec6bb551967a7e87df2c
Gabriel-otirB <lordglayder@gmail.com>
2025-02-16 22:28:01 -0300

    Implemented win condition and next word flow

--- src/App.jsx
+++ src/App.jsx
@@ -33,23 +33,22 @@ function App() {
   const [guesses, setGuesses] = useState(GUESSES_QUANTITY);
   const [score, setScore] = useState(0)
 
-  const pickWordAndCategory = () => {
+  const pickWordAndCategory = useCallback(() => {
     // pick a random category
     const categories = Object.keys(words);
     const category = categories[Math.floor(Math.random() * categories.length)]
 
-    console.log(category);
-
     // pick a random word
     const word = words[category][Math.floor(Math.random() * words[category].length)];
 
-    console.log(word);
-
     return { word, category };
-  }
+  }, [words]);
 
   // starts the secret word game
-  const startGame = () => {
+  const startGame = useCallback(() => {
+    // clear all letters
+    clearLetterStates();
+
     // pick word and pick category
     const { word, category } = pickWordAndCategory();
 
@@ -57,16 +56,13 @@ function App() {
     let wordLetters = word.split("");
     wordLetters = wordLetters.map((letra) => letra.toLowerCase());
 
-    console.log(word, category);
-    console.log(wordLetters);
-
     // fill states
     setPickedWord(word);
     setPickedCategory(category);
     setLetters(wordLetters);
 
     setGameStage(stages[1].name);
-  }
+  }, [pickWordAndCategory]);
 
   // process the letter input
   const verifyLetter = (letter) => {
@@ -93,9 +89,10 @@ function App() {
   }
 
   // DidUpdate // Quando Atualizar
+  // check if guesses ended
   useEffect(() => {
 
-    if(guesses <= 0) {
+    if (guesses <= 0) {
       // reset all states
       clearLetterStates();
 
@@ -103,6 +100,21 @@ function App() {
     }
 
   }, [guesses])
+
+  // check win condition
+  useEffect(() => {
+    const uniqueLetters = [...new Set(letters)];
+
+    // win condition
+    if(guessedLetters.length === uniqueLetters.length) {
+      // add score
+      setScore(prevScore => prevScore += 100);
+
+      // restart word
+      startGame();
+    }
+
+  }, [guessedLetters, letters, startGame]);
 
   // restarts the game
   const retry = () => {
